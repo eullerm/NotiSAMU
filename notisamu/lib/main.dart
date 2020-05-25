5abb685d14e2d35f54ed5fb22a09f02af5ac3c62
Euller Macena <euller_macena@id.uff.br>
2020-05-25 16:09:30 -0300

    Update main.dart

--- notisamu/lib/main.dart
+++ notisamu/lib/main.dart
@@ -14,7 +14,7 @@ void main()  {
 
   var app_secret = ios ? "7eb2d2ef-3b41-4ca5-a150-8bf0f373821e" : "78959eae-23d2-4b96-b122-ca99893bf1a0";
 
-  await AppCenter.start(app_secret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
+  AppCenter.start(app_secret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
 
   runApp(MyApp());
 }
