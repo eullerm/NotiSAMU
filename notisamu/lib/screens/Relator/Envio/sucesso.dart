e9545730ad88a4366a30d813de25e12ecf4bb530
Euller Macena <euller_macena@id.uff.br>
2020-06-08 16:42:06 -0300

    Revert "Revert "Mudanças no map de incidente""
    This reverts commit 06d526a09a3734142450b31e1aafdf1a4272d0d4.

--- notisamu/lib/screens/Relator/Envio/sucesso.dart
+++ notisamu/lib/screens/Relator/Envio/sucesso.dart
@@ -14,22 +14,15 @@ class Sucesso extends StatefulWidget {
 }
 
 class _SucessoState extends State<Sucesso> {
-  
   final database = Firestore.instance;
   final _fromKey = GlobalKey<FormState>();
+  bool send = false;
 
   @override
   void initState() {
     super.initState();
-    
-    //_addData();
 
-    Timer(
-        Duration(seconds: 2),
-        () => Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => Relator()),
-              ModalRoute.withName('/'),
-            ));
+    _addData();
   }
 
   @override
@@ -39,11 +32,11 @@ class _SucessoState extends State<Sucesso> {
         backgroundColor: Colors.red,
         title: Text("Dados especificos."),
       ),
-      body: _addData() ? Center( child: CircularProgressIndicator(),) :_body(context),
+      body: !send ? _progress(context) : _sended(context),
     );
   }
 
-  _body(context) {
+  _sended(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16),
       child: Column(
@@ -67,7 +60,33 @@ class _SucessoState extends State<Sucesso> {
     );
   }
 
-  _texto(string) {
+  _progress(context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.grey,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+                  strokeWidth: 2, //largura do cirulo
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _texto("Enviando."),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _texto(String string) {
     return Text(
       string,
       style: TextStyle(
@@ -77,21 +96,36 @@ class _SucessoState extends State<Sucesso> {
   }
 
   _addData() async {
-
-    await database.collection("notification").document().setData({
-      'notifying': this.widget.notificacao.notifying,
-      'profission': this.widget.notificacao.profission,
-      'patient': this.widget.notificacao.patient,
-      'birth': this.widget.notificacao.birth,
-      'sex': this.widget.notificacao.sex,
-      'occurrenceNumber': this.widget.notificacao.occurrenceNumber,
-      'local': this.widget.notificacao.local,
-      'occurrenceDate': this.widget.notificacao.occurrenceDate,
-      'period': this.widget.notificacao.period,
-      'incident': this.widget.notificacao.incident,
-      'answer': this.widget.notificacao.answer,
-      'infoExtra': this.widget.notificacao.infoExtra,
-    });
-
+    await database
+        .collection("notification")
+        .document()
+        .setData({
+          'notifying': this.widget.notificacao.notifying,
+          'profission': this.widget.notificacao.profission,
+          'patient': this.widget.notificacao.patient,
+          'birth': this.widget.notificacao.birth,
+          'sex': this.widget.notificacao.sex,
+          'occurrenceNumber': this.widget.notificacao.occurrenceNumber,
+          'local': this.widget.notificacao.local,
+          'occurrenceDate': this.widget.notificacao.occurrenceDate,
+          'period': this.widget.notificacao.period,
+          'incident': this.widget.notificacao.incident,
+          'answer': this.widget.notificacao.answer,
+          'infoExtra': this.widget.notificacao.infoExtra,
+        })
+        .timeout(Duration(seconds: 10))
+        .whenComplete(
+          () => setState(() {
+            send = true;
+            Timer(
+              Duration(seconds: 2),
+              () => Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => Relator()),
+                ModalRoute.withName('/'),
+              ),
+            );
+          }),
+        )
+        .catchError((error) => print("${error}"));
   }
 }
