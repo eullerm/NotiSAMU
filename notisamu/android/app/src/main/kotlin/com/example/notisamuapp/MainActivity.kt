d5e5ae6c25e3a69a6cd7badd9ff097a9ef03e42c
Euller Macena <49864491+eullerm@users.noreply.github.com>
2021-05-25 19:07:56 -0300

    Mudanças feitas para ser possivel build do apk

--- notisamu/android/app/src/main/kotlin/com/example/notisamuapp/MainActivity.kt
+++ notisamu/android/app/src/main/kotlin/com/example/notisamuapp/MainActivity.kt
@@ -4,4 +4,10 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+    }
+
 }
